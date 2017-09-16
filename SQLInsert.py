74a1d2c1bc58ee7f40475e0f5e63c97240c6ecb9
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-16 12:16:03 -0700

    Update SQLInsert.py

--- SQLInsert.py
+++ SQLInsert.py
@@ -1,1 +1,13 @@
 
+# This code attempts to insert a new row into the database, but doesn't
+# commit the insertion.  Add a commit call in the right place to make
+# it work properly.
+# 
+
+import sqlite3
+
+db = sqlite3.connect("testdb") #connect to databse
+c = db.cursor() #caall the curser
+c.execute("insert into balloons values ('blue', 'water') ") #use curser to execute query
+db.commit() #saves changes
+db.close() #disconnect database
