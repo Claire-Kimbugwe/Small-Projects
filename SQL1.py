36200fa43597460669bfb3803ed3bb6e049faf95
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-16 11:38:58 -0700

    Update SQL1.py

--- SQL1.py
+++ SQL1.py
@@ -1,1 +1,26 @@
 
+# connect to students data base and select students make sure
+# that the student records are fetched in sorted order
+# by student's name.
+
+
+import sqlite3
+
+# Fetch some student records from the database.
+db = sqlite3.connect("students")
+c = db.cursor()
+query = "select name, id from students order by name;"
+c.execute(query)
+rows = c.fetchall()
+
+# First, what data structure did we get?
+print "Row data:"
+print rows
+
+# And let's loop over it too:
+print
+print "Student names:"
+for row in rows:
+  print "  ", row[0]
+
+db.close()
