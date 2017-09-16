8a4770056c23ad69bfe2866f82a7b3c8add415b5
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-16 11:56:35 -0700

    Update SQL1.py

--- SQL1.py
+++ SQL1.py
@@ -1,4 +1,4 @@
-  
+##  
 # connect to students data base and select students make sure
 # that the student records are fetched in sorted order
 # by student's name.
@@ -7,11 +7,11 @@
 import sqlite3
 
 # Fetch some student records from the database.
-db = sqlite3.connect("students")
-c = db.cursor()
-query = "select name, id from students order by name;"
-c.execute(query)
-rows = c.fetchall()
+db = sqlite3.connect("students") #connect to db
+c = db.cursor()  #cursor used to run querries
+query = "select name, id from students order by name;"  #query
+c.execute(query) #executes query
+rows = c.fetchall() #use cursor to fetch the results(rows)
 
 # First, what data structure did we get?
 print "Row data:" #print the words raw data
