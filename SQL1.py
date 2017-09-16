ffc74aafef9a00b64ce40b707c46619a685b9fa2
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-16 11:46:39 -0700

    Update SQL1.py

--- SQL1.py
+++ SQL1.py
@@ -1,4 +1,4 @@
-
+  
 # connect to students data base and select students make sure
 # that the student records are fetched in sorted order
 # by student's name.
@@ -14,13 +14,13 @@ c.execute(query)
 rows = c.fetchall()
 
 # First, what data structure did we get?
-print "Row data:"
-print rows
+print "Row data:" #print the words raw data
+print rows        #prindts the rows fron db
 
 # And let's loop over it too:
-print
-print "Student names:"
-for row in rows:
+print        #empty line is printed
+print "Student names:" #print enclosed words
+for row in rows:   #iterate and print first column
   print "  ", row[0]
 
-db.close()
+db.close() #close connecction to db
